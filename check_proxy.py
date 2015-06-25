e235ad4f31afa4156432a49fd49b6deb40db70cc
iaston <maston@126.com>
2015-06-25 14:04:44 +0800

    v 0.1, can work now

---
+++ check_proxy.py
@@ -0,0 +1,210 @@
+# coding=utf-8
+# __author__ = 'maston'
+
+import threading
+import time
+import tkinter
+import requests
+import requests.exceptions
+import requests.adapters
+import maston
+import datetime
+import queue
+
+
+def update_gui():
+    global g_all_statu, g_lab_processing, g_text_valided_proxies
+    while g_btn_Valid_proxies.cget("state") == "disabled":
+        try:
+            time.sleep(0.8)
+            g_all_statu["lab_processing"]["num_thread"] = 0
+            for each_thread in threading.enumerate():
+                if each_thread.name.find("Verify_Proxy_") == 0:
+                    g_all_statu["lab_processing"]["num_thread"] += 1
+
+            valided_num = len(proxies_valided_list)
+            already_done_num = len(proxies_unvalided_list) - g_proxy_queue.qsize()
+            successful_rate = len(proxies_valided_list) * 100.0 / (already_done_num + 0.1)  # 加0.1为了最初时不会被0除
+            speed = already_done_num / ((datetime.datetime.now() - g_start_time).seconds + 0.01)
+            g_all_statu["lab_processing"]["valided_num"] = valided_num
+            g_all_statu["lab_processing"]["already_done_num"] = already_done_num
+            g_all_statu["lab_processing"]["successful_rate"] = successful_rate
+            g_all_statu["lab_processing"]["speed"] = speed
+
+            g_lab_processing.config(g_lab_processing,
+                                    text=maston.now() + "  有效{0} 已验{1} 有效率{2:.1f} 速度{3:.1f} 线程数{4}".format(
+                                        g_all_statu["lab_processing"]["valided_num"],
+                                        g_all_statu["lab_processing"]["already_done_num"],
+                                        g_all_statu["lab_processing"]["successful_rate"],
+                                        g_all_statu["lab_processing"]["speed"],
+                                        g_all_statu["lab_processing"]["num_thread"]))
+            g_text_valided_proxies.insert(1.0, "".join(g_all_statu["text_proxy_valid_append"]))
+            g_all_statu["text_proxy_valid_append"] = ""
+        except Exception as e:
+            print("GUI ERROR:\n" + repr(e) + "\n")
+        finally:
+            redraw_gui_event_finished.set()
+
+
+def get_a_proxy():
+    global g_b_stop, g_all_statu, g_proxy_queue
+    if g_b_stop:  # 如果有全局停止的信号，则返回空字符串让各个线程退出
+        return ""
+
+    if not g_proxy_queue.empty():
+        return str(g_proxy_queue.get())
+    else:
+        return ""
+
+
+def valid_proxy(check_site_info, success_try):
+    global lock_valided_list
+    i_error_limit = success_try[1] - success_try[0]
+    if i_error_limit < 0:
+        i_error_limit = 0
+    i_error_now = 0
+    proxy_now = get_a_proxy()
+    while proxy_now != "":
+        for each_check_site in check_site_info:  # 用每个站点来测试代理
+            # each_check_site内容为{'url':string, 'keyword':string, 'timeout':int}
+            i_error_now = 0
+            for iCounter in range(0, success_try[1]):  # 循环若干次测试代理，如错误超过上限，退出测试
+                html_result = ""
+                if (datetime.datetime.now() - g_gui_last_update_time).seconds > g_gui_update_interval:
+                    redraw_gui_event_finished.clear()
+                redraw_gui_event_finished.wait()  # 如果需要处理GUI，等待处理完成后再继续
+                try:
+                    read_timeout = int(check_site_info[each_check_site]['timeout'])
+                    connect_timeout = read_timeout / 2
+                    if connect_timeout < 2:
+                        connect_timeout = 2
+                    req_result = requests.get(check_site_info[each_check_site]['url'],
+                                              timeout=(connect_timeout, read_timeout),
+                                              proxies={'http': proxy_now})
+                    html_result = str(req_result.text.encode(req_result.encoding))
+                except Exception as e:
+                    print("\nError in proxy %s:\n%r" % (proxy_now, e))
+                if html_result.find(check_site_info[each_check_site]['keyword']) < 0:
+                    i_error_now += 1
+                    if i_error_now > i_error_limit:
+                        print(('!' * 10 + maston.now() + '!' * 10 + '\nInvalided proxy: ' + proxy_now))
+                        break  # 对应的是  for iCounter in range(0, success_try[1]): 即不再进行该测试站点的余下测试了
+            if i_error_now > i_error_limit:
+                break  # 对应的是  for each_check_site in check_site_info:  即不再进行余下测试站点的测试了，该代理不合格
+        if i_error_now <= i_error_limit:
+            lock_valided_list.acquire()
+            try:
+                proxies_valided_list.index(proxy_now)
+            except ValueError:
+                # 通过全部测试并与之前测试的不重复，表明该代理合格，保存到有效代理列表中
+                proxies_valided_list.append(proxy_now)
+                try:
+                    g_all_statu["text_proxy_valid_append"] += proxy_now + "\n"
+                    # g_text_valided_proxies.insert(1.0, proxy_now + "\n")  # 实时显示在text中
+                except Exception as e2:
+                    print("""g_all_statu["text_proxy_valid_append"] is wrong:\n""" + repr(e2))
+
+            lock_valided_list.release()
+
+        time.sleep(1)
+        proxy_now = get_a_proxy()
+    print("Finish, thread exit")
+
+
+def thread_monitor(check_site_info, success_try, th_num):
+    # 准备待查proxies的队列
+    for each_proxy in proxies_unvalided_list:
+        g_proxy_queue.put(each_proxy)
+
+    th_gui = threading.Thread(target=update_gui)
+    th_gui.setDaemon(True)
+    th_gui.start()
+
+    ths_verify = []
+    for iCounter in range(0, th_num):
+        t = threading.Thread(target=valid_proxy, args=(check_site_info, success_try))
+        t.setDaemon(True)
+        t.setName("Verify_Proxy_" + str(iCounter))
+        t.start()
+        ths_verify.append(t)
+
+    for iCounter in range(0, th_num):
+        ths_verify[iCounter].join()
+
+        # 隔几秒检查一次所有线程是否alive，如果全部退出了，就monitor退出，否则打印当前线程数量
+        # num_thread_now = th_num
+        # time.sleep(10)
+        # while num_thread_now > 0:
+        #     num_thread_now = 0
+        #     for iCounter in range(0, th_num):
+        #         if ths_verify[iCounter].isAlive():
+        #             num_thread_now += 1
+        #     if num_thread_now > 0:
+        #         print "*" * 20 + "\n" * 3 + maston.now() + "  : " + str(num_thread_now) + "\n" * 3
+        #         g_all_statu["lab_processing"]["num_thread"] = num_thread_now
+        #         # g_lab_processing.config(g_lab_processing, text=maston.now() + "  : " + str(num_thread_now))
+        #         time.sleep(5)
+        #     else:
+        #         print "*" * 20 + "\n\n All finished."
+
+
+def check_proxy(proxies_list, check_site_info, success_try, threads,
+                lab_processing, text_valided_proxies, btn_valided_proxies):
+    """
+    输入proxies_list(待检测的代理列表)和check_site_info(检测用网站的信息字典），检测标准为元组success_try(成功次数，总尝试次数），
+    运行线程数量为：threads, 运行的进度显示在lab_processing中。
+    check_site_info格式为：{'site1_name':{'url':string, 'keyword':string, 'timeout':int}, ...}
+    返回proxies_valided_list(测试可用的代理列表)
+    :param proxies_list: 待检测的代理列表
+    :param check_site_info: 检测用网站的信息字典
+    :param success_try: 元组(成功次数，总尝试次数）
+    :param threads: 运行线程数量
+    :param lab_processing: 显示检测进度的lable
+    :return:
+    """
+    global proxies_unvalided_list, g_lab_processing, proxies_valided_list, g_text_valided_proxies, g_btn_Valid_proxies
+    global g_proxy_list_pointer, g_start_time
+
+    proxies_unvalided_list = proxies_list
+    g_lab_processing = lab_processing
+    g_text_valided_proxies = text_valided_proxies
+    g_btn_Valid_proxies = btn_valided_proxies
+    proxies_valided_list = []
+    g_start_time = datetime.datetime.now()
+    g_btn_Valid_proxies.config(state=tkinter.DISABLED)
+
+    thread_monitor(check_site_info, success_try, threads)
+
+    # proxies_valided_list = list(set(proxies_valided_list))  # 返回前去重
+    g_btn_Valid_proxies.config(state=tkinter.NORMAL)
+    g_text_valided_proxies.delete(1.0, tkinter.END)
+    g_text_valided_proxies.insert(1.0, "\n".join(proxies_valided_list))
+    g_proxy_list_pointer = -1
+    return proxies_valided_list
+
+
+g_proxy_queue = queue.Queue(0)
+lock_valided_list = threading.Lock()
+redraw_gui_event_finished = threading.Event()
+proxies_unvalided_list = []
+proxies_valided_list = []
+
+g_gui_last_update_time = datetime.datetime.now()
+g_gui_update_interval = 1
+
+g_lab_processing = None
+g_text_valided_proxies = None
+g_btn_Valid_proxies = None
+g_b_stop = False
+g_start_time = datetime.datetime.now()
+g_all_statu = {"text_proxy_valid_append": "",
+               "lab_processing": {"valided_num": 0, "already_done_num": 0, "successful_rate": 0, "speed": 0,
+                                  "num_thread": 0}}
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import tkinter.ttk as ttk
+
+
+class McListBox:
+    """use a ttk.TreeView as a multicolumn ListBox"""
+
+    def __init__(self, table_header=None, table_list=None):
+        self.table_header = table_header
+        self.table_list = table_list
+        self.tree = None
+        self._setup_widgets()
+        self._build_tree()
+
+    def _setup_widgets(self):
+        self.container = ttk.Frame()
+        self.container.pack(fill='both', expand=False)
+        # create a treeview with dual scrollbars
+        self.tree = ttk.Treeview(columns=self.table_header, show="headings")
+        self.vsb = ttk.Scrollbar(orient="vertical",
+                                 command=self.tree.yview)
+        self.hsb = ttk.Scrollbar(orient="horizontal",
+                                 command=self.tree.xview)
+        self.tree.configure(yscrollcommand=self.vsb.set,
+                            xscrollcommand=self.hsb.set)
+        self.tree.grid(column=0, row=0, sticky='nsew', in_=self.container)
+        self.vsb.grid(column=1, row=0, sticky='ns', in_=self.container)
+        self.hsb.grid(column=0, row=1, sticky='ew', in_=self.container)
+
+        self.container.grid_columnconfigure(0, weight=1)
+        self.container.grid_rowconfigure(0, weight=1)
+
+    def _build_tree(self):
+        for col in self.table_header:
+            self.tree.heading(col, text=col.title(),
+                              command=lambda c=col: sortby(self.tree, c, 0))
+            # adjust the column's width to the header string
+            self.tree.column(col,
+                             width=tkFont.Font().measure(col.title()))
+
+        for item in self.table_list:
+            self.tree.insert('', 'end', values=item)
+            # adjust column's width if necessary to fit each value
+            for ix, val in enumerate(item):
+                col_w = tkFont.Font().measure(val)
+                if self.tree.column(self.table_header[ix], width=None) < col_w:
+                    self.tree.column(self.table_header[ix], width=col_w)
+
+                    # print(self.container.width)
+
+    def rebuild_tree(self):
+        self.tree.configure(columns=self.table_header)
+        self._build_tree()
+
+    def _set_header(self, header=None):
+        self.table_header = header
+
+    def _set_list(self, list1=None):
+        self.table_list = list1
+
+
+def sortby(tree, col, descending):
+    """sort tree contents when a column header is clicked on"""
+    # grab values to sort
+    data = [(tree.set(child, col), child) \
+            for child in tree.get_children('')]
+    # if the data to be sorted is numeric change to float
+    # data =  change_numeric(data)
+    # now sort the data in place
+    data.sort(reverse=descending)
+    for ix, item in enumerate(data):
+        tree.move(item[1], '', ix)
+    # switch the heading so it will sort in the opposite direction
+    tree.heading(col, command=lambda col=col: sortby(tree, col, \
+                                                     int(not descending)))
+
+
+if __name__ == "__main__":
+    # the test data ...
+    car_header = ['car', 'repair']
+    car_list = [
+        ('Hyundai', 'brakes'),
+        ('Honda', 'light'),
+        ('Lexus', 'battery'),
+        ('Benz', 'wiper'),
+        ('Ford', 'tire'),
+        ('Chevy', 'air'),
+        ('Chrysler', 'piston'),
+        ('Toyota', 'brake pedal'),
+        ('BMW', 'seat')
+    ]
+    root = tk.Tk()
+    root.wm_title("multicolumn ListBox")
+    mc_listbox = McListBox(table_header=car_header, table_list=car_list)
+
+    def update_table():
+        for i in map(mc_listbox.tree.delete, mc_listbox.tree.get_children('')):
+            pass
+        mc_listbox.rebuild_tree()
+        print(mc_listbox.tree.winfo_width())
+
+    tk.Button(root, text="update", command=update_table).pack()
+    root.mainloop()
